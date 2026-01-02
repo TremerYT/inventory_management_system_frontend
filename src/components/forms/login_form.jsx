3726f6c0813af5a76b4c563bf7411f7997067583
TremerYT <jefferymutuku283@gmail.com>
2026-01-02 16:05:15 +0300

    Changes Made - Created the Finished sign-in page

--- src/components/forms/login_form.jsx
+++ src/components/forms/login_form.jsx
@@ -1,31 +1,40 @@
-import {Button, Form, Input, Space} from "antd";
-import {EyeInvisibleOutlined, EyeTwoTone} from "@ant-design/icons";
-
+import {Button, Checkbox, Form, Input, Space} from "antd";
 
 const LoginForm = () => {
   const [loginForm] = Form.useForm();
   return (
       <Form form={loginForm} layout="vertical">
         <Form.Item
-          rules={[
-            { required: true, message: "Email address is required" },
-            { type: "email", message: "Enter a valid email address" }
-          ]}
+          label="Email"
           name="email"
-          label="Email Address"
+          rules={[{ required: true, type: "email" }]}
         >
-          <Input placeholder = "Enter Email"/>
+          <Input size="large" placeholder="Enter your email" />
         </Form.Item>
+
         <Form.Item
-          rules={[{ required: true, message: "Password is required" }]}
-          name="password"
           label="Password"
+          name="password"
+          rules={[{ required: true }]}
         >
-          <Input.Password
-            placeholder = "Enter Password"
-            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined/>)}
-          />
+          <Input.Password size="large" placeholder="Enter your password" />
         </Form.Item>
+
+        <div className="flex justify-between items-center mb-4">
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Stay logged in on this device</Checkbox>
+          </Form.Item>
+          <Button type="link" size="large">Forgot Password?</Button>
+        </div>
+
+        <Button
+          type="primary"
+          htmlType="submit"
+          size="large"
+          className="w-full mb-6"
+        >
+          Login
+        </Button>
       </Form>
   );
 };
--- src/components/layout/auth_layout.jsx
+++ src/components/layout/auth_layout.jsx
@@ -1,18 +1,17 @@
-import { Button, Card } from "antd";
+import {Button, Divider} from "antd";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <div className="flex gap-10 p-10 shadow-lg max-w-4xl w-full">
-        <div className="flex-1">
-          <h1 className="mb-10 font-bold">Nirvana</h1>
+    <div className="min-h-screen flex justify-center items-center bg-gray-300">
+      <div className="flex shadow-lg max-w-5xl w-full bg-gray-200 rounded-2xl" >
+        <div className="flex-1 px-10 py-10 w-1/2">
+          <h1 className="mb-30">Nirvana</h1>
           <div className="flex flex-col justify-center text-center mb-10">
-            <h2 className="text-xl font-semibold">Login to your account</h2>
+            <h2 className="text-xl">Login to your account</h2>
             <p className="text-gray-500">Welcome back, please enter your details</p>
           </div>
-
           <div className="flex gap-4 w-full mb-6">
             <Button icon={<FcGoogle />} block size="large">
               Continue with Google
@@ -26,16 +25,20 @@ const AuthLayout = ({ children }) => {
               Continue with Facebook
             </Button>
           </div>
-
+          <Divider plain>OR</Divider>
           <div>
             {children}
           </div>
+
+          <div className="flex items-center justify-center">
+            <p>Not registered yet?</p>
+            <Button type="link" size="large">Create an account</Button>
+          </div>
         </div>
-        <div className="flex-1 flex justify-center items-center">
+        <div className="w-1/2 bg-gray-500 flex items-center justify-center rounded-r-2xl">
           <img
             src="/login.png"
             alt="login illustration"
-            className="max-w-full h-auto"
           />
         </div>
       </div>
